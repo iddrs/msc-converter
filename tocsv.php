ce2538f7a3a7e451a316f62c38dba52fc1172085
Everton da Rosa <everton3x@gmail.com>
2020-10-26 10:34:38 -0300

    Adicionado montagem do bal rec

--- tocsv.php
+++ tocsv.php
@@ -13,7 +13,6 @@ $handle = fopen($msc_file, 'r');
 $msc = read_csv($handle, ';', true, 1);
 fclose($handle);
 //print_r($msc);
-
 //balancete contábil
 echo "Criando o Balancete Contábil:", PHP_EOL;
 //cria a estrutura de dados convertidos
@@ -21,13 +20,13 @@ $bal_cont_data = [];
 
 //inicia loop pelos dados
 
-foreach ($msc as $id => $row){
+foreach ($msc as $id => $row) {
     $bal_cont_data[$id]['ContaContabil'] = transf_cc($row['ContaContabil']);
     $bal_cont_data[$id]['Valor'] = $row['Valor'];
     $bal_cont_data[$id]['ValorF'] = number_format($row['Valor'], 2, ',', '.');
     $bal_cont_data[$id]['TipoValor'] = $row['TipoValor'];
     $bal_cont_data[$id]['NaturezaValor'] = $row['NaturezaValor'];
-    
+
     //inicialização das informações complementares
     $bal_cont_data[$id]['PoderOrgao'] = null;
     $bal_cont_data[$id]['FinanceiroPermanente'] = null;
@@ -39,14 +38,14 @@ foreach ($msc as $id => $row){
     $bal_cont_data[$id]['FuncaoSubfuncao'] = null;
     $bal_cont_data[$id]['AnoInscricaoRestosAPagar'] = null;
     $bal_cont_data[$id]['DespesasMDEeASPS'] = null;
-    
+
     //identifica as informações complementares
-    for($i = 1; $i <= 7; $i++){
+    for ($i = 1; $i <= 7; $i++) {
         $colname = "TipoInformacaoComplementar$i";
         $TipoIC = $row[$colname];
         $ValorIC = $row["InformacaoComplementar$i"];
 //        var_dump($ValorIC);exit();
-        switch ($TipoIC){
+        switch ($TipoIC) {
             case 'PO':
                 $destiny = 'PoderOrgao';
                 break;
@@ -89,7 +88,6 @@ foreach ($msc as $id => $row){
     }
 }
 //print_r($output);
-
 //salva o conteúdo
 $handle = fopen(join_path($output_dir, $bal_cont_file), 'w');
 write_csv($handle, $bal_cont_data, ';', true);
@@ -102,3 +100,105 @@ echo "Criando o Balancete da Receita:", PHP_EOL;
 
 $bal_rec_data = [];
 
+//loop sobre os dados contábeis
+foreach ($bal_cont_data as $id => $row) {
+    if (strlen($row['NaturezaReceita']) === 0) {
+        continue;
+    }
+    
+    $nro = $row['NaturezaReceita'];
+    
+    if(!key_exists($nro, $bal_rec_data)){
+        $bal_rec_data[$nro] = [
+            'PrevisaoInicialBruta' => 0,
+            'PrevisaoInicialDeducoes' => 0,
+            'AlteracoesBruta' => 0,
+            'AlteracoesDeducoes' => 0,
+            'ArrecadadoBruto' => 0,
+            'ArrecadadoDeducoes' => 0
+        ];
+    }
+    
+    //previsão inicial
+    if($row['ContaContabil'] === '5.2.1.1.1.00.00'){//previsão inicial bruta
+        if($row['TipoValor'] === 'ending_balance'){
+            $bal_rec_data[$nro]['PrevisaoInicialBruta'] += $row['Valor'];
+        }
+    }
+    if(//deduções da previsão inicial
+            $row['ContaContabil'] === '5.2.1.1.2.01.01'
+            || $row['ContaContabil'] === '5.2.1.1.2.01.02'
+            || $row['ContaContabil'] === '5.2.1.1.2.99.00'
+        ){
+        if($row['TipoValor'] === 'ending_balance'){
+            $bal_rec_data[$nro]['PrevisaoInicialDeducoes'] += $row['Valor'];
+        }
+    }
+    
+    //alteração da previsão
+    if(
+            $row['ContaContabil'] === '5.2.1.2.1.01.00'
+            || $row['ContaContabil'] === '5.2.1.2.1.02.00'
+        ){//reestimativa
+        if($row['TipoValor'] === 'ending_balance'){
+            $bal_rec_data[$nro]['AlteracoesBruta'] += $row['Valor'];
+        }
+    }
+    if(//alteração das deduções
+            $row['ContaContabil'] === '5.2.1.2.1.03.01'
+            || $row['ContaContabil'] === '5.2.1.2.1.03.02'
+            || $row['ContaContabil'] === '5.2.1.2.1.04.00'
+            || $row['ContaContabil'] === '5.2.1.2.1.99.00'
+        ){
+        if($row['TipoValor'] === 'ending_balance'){
+            $bal_rec_data[$nro]['AlteracoesDeducoes'] += $row['Valor'];
+        }
+    }
+    
+    //arrecadação
+    if(
+            $row['ContaContabil'] === '6.2.1.2.0.00.00'
+        ){
+        if($row['TipoValor'] === 'ending_balance'){
+            $bal_rec_data[$nro]['ArrecadadoBruto'] += $row['Valor'];
+        }
+    }
+    
+    if(
+            $row['ContaContabil'] === '6.2.1.3.1.01.00'
+            || $row['ContaContabil'] === '6.2.1.3.1.02.00'
+            || $row['ContaContabil'] === '6.2.1.3.2.00.00'
+            || $row['ContaContabil'] === '6.2.1.3.9.00.00'
+        ){
+        if($row['TipoValor'] === 'ending_balance'){
+            $bal_rec_data[$nro]['ArrecadadoDeducoes'] += $row['Valor'];
+        }
+    }
+    
+}
+
+//transpões os dados para o formato tabular
+$output = [];
+foreach ($bal_rec_data as $nro => $data){
+    $output[] = [
+        'NaturezaReceita' => $nro,
+        'PrevisaoInicialBruta' => number_format($data['PrevisaoInicialBruta'], 2, ',', '.'),
+        'PrevisaoInicialDeducoes' => number_format($data['PrevisaoInicialDeducoes'], 2, ',', '.'),
+        'PrevisaoInicialLiquida' => number_format($data['PrevisaoInicialBruta'] - $data['PrevisaoInicialDeducoes'], 2, ',', '.'),
+        'AlteracoesBruta' => number_format($data['AlteracoesBruta'], 2, ',', '.'),
+        'AlteracoesDeducoes' => number_format($data['AlteracoesDeducoes'], 2, ',', '.'),
+        'AlteracoesLiquida' => number_format($data['AlteracoesBruta'] - $data['AlteracoesDeducoes'], 2, ',', '.'),
+        'PrevisaoAtualizadaBruta' => number_format($data['PrevisaoInicialBruta']+$data['AlteracoesBruta'], 2, ',', '.'),
+        'PrevisaoAtualizadaDeducoes' => number_format($data['PrevisaoInicialDeducoes']+$data['AlteracoesDeducoes'], 2, ',', '.'),
+        'PrevisaoAtualizadaLiquida' => number_format($data['PrevisaoInicialBruta'] - $data['PrevisaoInicialDeducoes'] + $data['AlteracoesBruta'] - $data['AlteracoesDeducoes'], 2, ',', '.'),
+        'ArrecadadoBruto' => number_format($data['ArrecadadoBruto'], 2, ',', '.'),
+        'ArrecadadoDeducoes' => number_format($data['ArrecadadoDeducoes'], 2, ',', '.'),
+        'ArrecadadoLiquido' => number_format($data['ArrecadadoBruto'] - $data['ArrecadadoDeducoes'], 2, ',', '.'),
+    ];
+}
+
+//salva os dados
+$handle = fopen(join_path($output_dir, $bal_rec_file), 'w');
+write_csv($handle, $output, ';', true);
+fclose($handle);
+echo "\tlinhas criadas: ", sizeof($output), PHP_EOL;
